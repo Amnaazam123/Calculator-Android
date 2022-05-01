bb8a8e370a9c9c82352232ca4cd539795afdd503
Amnaazam123 <67511883+Amnaazam123@users.noreply.github.com>
2022-05-01 23:38:23 +0500

    click functionality done

--- app/src/main/java/com/example/calculator_android/MainActivity.java
+++ app/src/main/java/com/example/calculator_android/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.calculator_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
+            buttonMul, buttonEqual;
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonAdd = (Button) findViewById(R.id.buttonadd);
+        buttonSub = (Button) findViewById(R.id.buttonsub);
+        buttonMul = (Button) findViewById(R.id.buttonmul);
+        buttonDivision = (Button) findViewById(R.id.buttondiv);
+        buttonEqual = (Button) findViewById(R.id.buttoneql);
+        t = (TextView) findViewById(R.id.txt);
+    }
+    public void onButtonClick(View view){
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+        String myTxt = t.getText().toString();
+        t.setText(myTxt+ buttonText);
     }
 }
